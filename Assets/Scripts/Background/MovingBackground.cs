8b942b4fa1e38f4b86247fa05fa85f5a0e7eee8b
Tilman Raendchen <TReandchen@outlook.com>
2018-11-17 11:47:17 +0000

    Add moving background script

---
+++ Assets/Scripts/Background/MovingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    public float ScrollSpeed = 1f;
+    Renderer rend;
+
+    void Start()
+    {
+        this.rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * (this.ScrollSpeed / 10);
+        this.rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
