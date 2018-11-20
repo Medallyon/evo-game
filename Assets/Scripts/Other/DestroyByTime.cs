3150af1a3130e15817c4cdb51d56622aa435f2ec
Tilman Raendchen <TReandchen@outlook.com>
2018-11-20 00:27:42 +0000

    Create some util scripts

---
+++ Assets/Scripts/Other/DestroyByTime.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Destroys this object after an amount of seconds
+public class DestroyByTime : MonoBehaviour
+{
+    public float LifeTime;
+
+	// Use this for initialization
+	void Start()
+    {
+        Destroy(this.gameObject, this.LifeTime);
+	}
+}
