d8e10bd58be7e4d4f7b190ab626865634a219a67
Tilman Raendchen <TReandchen@outlook.com>
2018-11-20 00:26:44 +0000

    Create basic 'Obstacle' prefab

---
+++ Assets/Scripts/Game Controller/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float Difficulty;
+    public GameObject Obstacle;
+    public Vector3 SpawnPosition;
+
+    private void SpawnWave()
+    {
+        Instantiate(this.Obstacle, new Vector3(Random.Range(-this.SpawnPosition.x, this.SpawnPosition.x), 1, this.SpawnPosition.z), Quaternion.identity);
+    }
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        while (GameObject.Find("Player") != null)
+        {
+            SpawnWave();
+            this.Difficulty += .1f;
+            yield return new WaitForSeconds(Mathf.Max(1f, Random.Range(0f, 5f) - this.Difficulty));
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacle/ObstacleMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody rb;
+    private Transform target;
+
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody>();
+        this.target = GameObject.Find("Player").transform;
+
+        this.transform.LookAt(this.target.transform);
+        this.rb.AddRelativeForce(Vector3.forward * this.Speed, ForceMode.Impulse);
+    }
+
+    //void Update()
+    //{
+    //    float step = this.Speed * Time.deltaTime;
+    //    this.transform.position = Vector3.MoveTowards(this.transform.position, this.target.position, step);
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        Destroy(this.gameObject);
+    }
+}
