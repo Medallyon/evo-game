e485e1938a16381ca94174656951e6b1b3c75d5b
Michalis Antonas <michalis-a@hotmail.co.uk>
2018-11-20 10:05:53 +0000

    Create random rotator script

---
+++ Assets/Scripts/Other/RotateRandomAxis.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRandomAxis : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.rb = this.GetComponent<Rigidbody>();
+
+        this.rb.angularVelocity = Random.insideUnitSphere;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
