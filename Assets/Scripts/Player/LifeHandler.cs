1b2466657adac77230ccc96ba459226dd6f2a7ea
Tilman Raendchen <TReandchen@outlook.com>
2018-11-20 00:27:16 +0000

    Create basic life & score handlers

---
+++ Assets/Scripts/Player/LifeHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeHandler : MonoBehaviour
+{
+    public int Lives = 5;
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Obstacle")
+            return;
+
+        this.DecreaseLives();
+    }
+
+    private void DecreaseLives()
+    {
+        this.Lives--;
+
+        // Implement UI-specific code (i.e. 5 -> 4 hearts)
+
+        if (this.Lives <= 0)
+        {
+            // End level and display Score
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/ScoreHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class ScoreHandler : MonoBehaviour
+{
+    #region Public Fields
+    public float ScoreToWin = 1000;
+    #endregion
+ 
+    #region Unity Methods
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Edible")
+            return;
+
+        // Implement Score-specific code
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
