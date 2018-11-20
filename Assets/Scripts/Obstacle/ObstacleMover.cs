e6d27622b7b8a7cabd66e18d7688b0b7cac872ae
Tilman Raendchen <TRaendchen@outlook.com>
2018-11-20 01:30:20 +0000

    Remove unused code

--- Assets/Scripts/Obstacle/ObstacleMover.cs
+++ Assets/Scripts/Obstacle/ObstacleMover.cs
@@ -1,41 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class ObstacleMover : MonoBehaviour
 {
     public float Speed;
-
-    private Rigidbody rb;
+    
     private Transform target;
     private int frames = 0;
     private Queue<Vector3> previousTargetPositions = new Queue<Vector3>();
-
-    //void Start()
-    //{
-    //    this.rb = this.GetComponent<Rigidbody>();
-    //    this.target = GameObject.Find("Player").transform;
-
-    //    this.transform.LookAt(this.target.transform);
-    //    this.rb.AddRelativeForce(Vector3.forward * this.Speed, ForceMode.Impulse);
-    //}
-
-    //void Update()
-    //{
-    //    this.frames++;
-    //    if (this.frames < 15)
-    //        return;
-
-    //    this.rb.velocity = Vector3.zero;
-    //    this.rb.angularVelocity = Vector3.zero;
-
-    //    if (this.transform.position.z > this.target.position.z + 5)
-    //        this.transform.LookAt(this.target.transform);
-    //    this.rb.AddRelativeForce(Vector3.forward * this.Speed, ForceMode.Impulse);
-
-    //    this.frames = 0;
-    //}
 
     void Start()
     {
