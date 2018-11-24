022ac9dbb7d2e67a6de2e483196c883ba1487505
Tilman Raendchen <TRaendchen@outlook.com>
2018-11-24 19:56:37 +0000

    Add scene loading functionality

---
+++ Assets/Scripts/Menu/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+ 
+public class LoadScene : MonoBehaviour
+{
+    #region Public Fields
+    public string Scene;
+    #endregion
+ 
+    #region Unity Methods
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(LoadGame);
+    }
+    #endregion
+
+    #region Private Methods
+    private void LoadGame()
+    {
+        Debug.Log("Scene loading: " + this.Scene);
+        SceneManager.LoadScene(this.Scene, LoadSceneMode.Single);
+    }
+    #endregion
+}
