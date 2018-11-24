6b3dbda55096dee4da66cf6ca807a1b51fe47642
Tilman Raendchen <TRaendchen@outlook.com>
2018-11-24 19:59:22 +0000

    Enable cross-compatible UI display positions

--- Assets/Scripts/Player/LifeHandler.cs
+++ Assets/Scripts/Player/LifeHandler.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+ 
 public class LifeHandler : MonoBehaviour
 {
+    #region Public Fields
     public int Lives = 5;
-    public Text textLives; 
+    public GameObject Canvas;
+    public Text TextPrefab;
+    #endregion
 
-	// Use this for initialization
-	void Start()
+    private Text textLives;
+
+    #region Unity Methods
+    void Start()
     {
+        GameObject TextBox = new GameObject("Text_Score");
+        TextBox.transform.parent = this.Canvas.transform;
+        this.textLives = TextBox.AddComponent<Text>();
+
+        Rect dims = this.GetCameraDimensions();
+        RectTransform BoxTransform = TextBox.GetComponent<RectTransform>();
+        BoxTransform.sizeDelta = new Vector2(200, 28);
+        TextBox.transform.localPosition = new Vector3(-((dims.width * Screen.width) / 2) + (BoxTransform.sizeDelta.x / 2) + 10, ((dims.height * Screen.height) / 2) - (BoxTransform.sizeDelta.y / 2) - 10);
+
+        this.textLives.color = this.TextPrefab.color;
+        this.textLives.font = this.TextPrefab.font;
+        this.textLives.fontSize = this.TextPrefab.fontSize;
+        this.textLives.fontStyle = this.TextPrefab.fontStyle;
+        this.textLives.lineSpacing = this.TextPrefab.lineSpacing;
+
+        Outline outline = this.textLives.gameObject.AddComponent<Outline>();
+        outline = this.TextPrefab.GetComponent<Outline>();
+
         this.textLives.text = "Lives: " + this.Lives;
-	}
-	
-	// Update is called once per frame
-	void Update()
-    {
-		
-	}
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -27,7 +44,9 @@ public class LifeHandler : MonoBehaviour
 
         this.DecreaseLives();
     }
+    #endregion
 
+    #region Private Methods
     private void DecreaseLives()
     {
         this.Lives--;
@@ -41,4 +60,37 @@ public class LifeHandler : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    private Rect GetCameraDimensions()
+    {
+        // determine the game window's current aspect ratio
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleheight = windowaspect / (9f / 16f);
+
+        Rect rect = new Rect();
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleheight < 1.0f)
+        {
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+        }
+
+        Debug.Log(rect.x + ", " + rect.y);
+        return rect;
+    }
+    #endregion
 }
--- Assets/Scripts/Player/ScoreHandler.cs
+++ Assets/Scripts/Player/ScoreHandler.cs
@@ -6,17 +6,37 @@ using UnityEngine.UI;
  
 public class ScoreHandler : MonoBehaviour
 {
-    private float currentScore = 0;
-
     #region Public Fields
     public float ScoreToWin = 1000;
-    public Text textScore;
+    public GameObject Canvas;
+    public Text TextPrefab;
     #endregion
+
+    private float currentScore = 0;
+    private Text textScore;
  
     #region Unity Methods
     void Start()
     {
-        
+        GameObject TextBox = new GameObject("Text_Score");
+        TextBox.transform.parent = this.Canvas.transform;
+        this.textScore = TextBox.AddComponent<Text>();
+
+        Rect dims = this.GetCameraDimensions();
+        RectTransform BoxTransform = TextBox.GetComponent<RectTransform>();
+        BoxTransform.sizeDelta = new Vector2(200, 28);
+        TextBox.transform.localPosition = new Vector3(-((dims.width * Screen.width) / 2) + (BoxTransform.sizeDelta.x / 2) + 10, ((dims.height * Screen.height) / 2) - (BoxTransform.sizeDelta.y * 1.5f) - 10);
+
+        this.textScore.color = this.TextPrefab.color;
+        this.textScore.font = this.TextPrefab.font;
+        this.textScore.fontSize = this.TextPrefab.fontSize;
+        this.textScore.fontStyle = this.TextPrefab.fontStyle;
+        this.textScore.lineSpacing = this.TextPrefab.lineSpacing;
+
+        Outline outline = this.textScore.gameObject.AddComponent<Outline>();
+        outline = this.TextPrefab.GetComponent<Outline>();
+
+        this.textScore.text = "Score: " + this.currentScore;
     }
 
     void OnTriggerEnter(Collider other)
@@ -33,5 +53,36 @@ public class ScoreHandler : MonoBehaviour
     #endregion
 
     #region Private Methods
+    private Rect GetCameraDimensions()
+    {
+        // determine the game window's current aspect ratio
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        float scaleheight = windowaspect / (9f / 16f);
+
+        Rect rect = new Rect();
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleheight < 1.0f)
+        {
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+        }
+
+        Debug.Log(rect.x + ", " + rect.y);
+        return rect;
+    }
     #endregion
 }
