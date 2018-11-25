ae8250f36fdd16015bc86e7926e583bb3da7a96a
Tilman Raendchen <TRaendchen@outlook.com>
2018-11-25 21:20:36 +0000

    Create PowerUp Prefab

---
+++ Assets/Scripts/PowerUps/PowerUpMover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+ 
+public class PowerUpMover : MonoBehaviour
+{
+    #region Public Fields
+    public float Speed = 1;
+    #endregion
+    
+    private Vector3 target;
+ 
+    #region Unity Methods
+    void Start()
+    {
+        Vector3 playerPosition = GameObject.Find("Player").transform.position;
+        this.target = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z);
+        this.target.Set(this.target.x, this.target.y, this.target.z - 10);
+    }
+ 
+    void Update()
+    {
+        float step = this.Speed * Time.deltaTime;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.target, step);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        ScoreHandler playerObject = FindObjectOfType<ScoreHandler>();
+        playerObject.ScoreMultiplier = 2;
+
+        // TODO: Instantiate(DOUBLE SCORE TEXT!!!);
+
+        Timer timer = new Timer()
+        {
+            Interval = 10000,
+            Enabled = true
+        };
+
+        timer.Elapsed += (object Source, ElapsedEventArgs e) =>
+        {
+            playerObject.ScoreMultiplier = 1;
+            timer.Enabled = false;
+        };
+
+        Destroy(this.gameObject);
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
---
+++ Assets/Scripts/PowerUps/RotateAroundAxis.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Axis { X, Y, Z };
+
+public class RotateAroundAxis : MonoBehaviour
+{
+    #region Public Fields
+    public Axis Axis = Axis.X;
+    public float Speed = 1;
+    #endregion
+
+    private Dictionary<Axis, float> velocities = new Dictionary<Axis, float>()
+    {
+        { Axis.X, 0 },
+        { Axis.Y, 0 },
+        { Axis.Z, 0 }
+    };
+    private Rigidbody rb;
+    private Vector3 eulerAngleVelocity;
+ 
+    #region Unity Methods
+    void Start()
+    {
+        this.velocities[this.Axis] = 100;
+        this.eulerAngleVelocity = new Vector3(this.velocities[Axis.X], this.velocities[Axis.Y], this.velocities[Axis.Z]);
+
+        this.rb = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(this.eulerAngleVelocity * Time.deltaTime * this.Speed);
+        this.rb.MoveRotation(this.rb.rotation * deltaRotation);
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
