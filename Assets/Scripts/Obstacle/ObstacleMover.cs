161760546b731817bb1b04f3fe9f52fb8301cfc9
Tilman Raendchen <TRaendchen@outlook.com>
2018-11-30 01:39:32 +0000

    Fix Shark Animation & Rotation

--- Assets/Scripts/Obstacle/ObstacleMover.cs
+++ Assets/Scripts/Obstacle/ObstacleMover.cs
@@ -28,6 +28,9 @@ public class ObstacleMover : MonoBehaviour
             targetPos = new Vector3(this.target.position.x, 1, this.target.position.z - 10);
         this.transform.position = Vector3.MoveTowards(this.transform.position, targetPos, step);
 
+        if (this.gameObject.scene.name.EndsWith("2"))
+            this.transform.localRotation = new Quaternion(0, 180, 0, 0);
+
         this.frames++;
         if (this.frames == 2)
         {
